f59b3796f7614e2332984282e10804292c222f12
Chi Kim <chi@kim.club>
2021-09-21 21:56:22 -0700

    fixes startup bugs

--- server.js
+++ server.js
@@ -1,38 +1,37 @@
-const express = require('express')
-const {Server} = require('socket.io')
-const http = require('http')
-const path = require('path')
-const app = express()
+const express = require("express");
+const { Server } = require("socket.io");
+const http = require("http");
+const path = require("path");
+const app = express();
 // const cors = require('cors')
 
 // app.use(cors())
-app.use(express.static("public"))
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname+'/public/index.html')
-})
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
 
 const server = http.createServer(app);
-const io = new Server(server)
+const io = new Server(server);
 
 io.on("connection", (socket) => {
-
   socket.on("join_room", (data) => {
-    socket.join(data)
-  })
+    socket.join(data);
+  });
 
   socket.on("send_message", (data) => {
-    socket.to(data.room).emit("receive_message", data)
-  })
+    socket.to(data.room).emit("receive_message", data);
+  });
 
-  socket.on("disconnect", () => {
-  })
-})
+  socket.on("disconnect", () => {});
+});
 
-server.listen(3001, () => {
+server.listen(process.env.PORT || 3001, () => {
   console.log("socket server running");
-})
+});
 
 // app.listen(3000, () => {
 //   console.log('SERVER RUNNING')
 // })
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,24 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: './src/app.js', // The first file to look into. Move your JavaScript here!
-  mode: 'production',
+  entry: "./src/App.js", // The first file to look into. Move your JavaScript here!
+  mode: "production",
   output: {
-    path: path.resolve(__dirname, 'public/dist'), // We will put the compiled file into public/dist
-    filename: "bundle.js"
+    path: path.resolve(__dirname, "public/dist"), // We will put the compiled file into public/dist
+    filename: "bundle.js",
   },
   module: {
     rules: [
-      { // This section tells Webpack to use Babel to translate your React into JavaScript
+      {
+        // This section tells Webpack to use Babel to translate your React into JavaScript
         test: /\.js$/, // Regex for all JavaScript file
         exclude: /node_modules/,
-        loader: 'babel-loader',
+        loader: "babel-loader",
         options: {
-          'presets': ['@babel/preset-react'],
+          presets: ["@babel/preset-react"],
         },
       },
     ],
   },
 };
+
